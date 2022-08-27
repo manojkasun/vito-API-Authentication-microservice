5dbbb9f4f659e0e132acb87b03208c8026c7a638
ManojKasun <manojkasun1996@gmail.com>
2022-08-27 10:36:55 +0530

    Add initial project to the git

---
+++ src/main/java/vitos/pzza/authpart/AuthPartApplication.java
@@ -0,0 +1,13 @@
+package vitos.pzza.authpart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthPartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthPartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vitos/pzza/authpart/AuthPartApplicationTests.java
@@ -0,0 +1,13 @@
+package vitos.pzza.authpart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthPartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
